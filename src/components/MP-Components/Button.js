d5018471eb9f2c2f50f4af31163959ff306f6dfe
Michael Plange <michaelpplange@gmail.com>
2018-05-22 14:17:23 -0500

    Basic Landing Page Template

---
+++ src/components/MP-Components/Button.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export default styled.button`
+  background-color: grey;
+  border: none;
+  outline: none;
+  color: white;
+  text-align: center;
+  margin: 4px 2px;
+  border-radius: 25px;
+  &:hover {
+    background-color: tomato;
+  }
+  width: 8vh;
+  height: 4vh;
+`;
