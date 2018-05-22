8f16b79ae0236d955a76e5934a5b8b65a9c2e4d7
des-tinyDM <destinyleaross@gmail.com>
2018-05-22 11:36:20 -0500

    folder layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { Route, Switch } from "react-router-dom";
+
+//IMPORT COMPONENTS
 import Landing from "./components/Landing/Landing";
+import Dashboard from "./components/Dashboard/Dashboard";
+
+//IMPORT ASSETS AND CSS
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Landing />
+        <Route
+          path="/"
+          render={() => (this.props.user ? <Dashboard /> : <Landing />)}
+        />
       </div>
     );
   }
