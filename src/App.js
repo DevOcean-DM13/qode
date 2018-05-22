767cd324c3cd9b527a9edbbf8db2b4b9111af937
des-tinyDM <destinyleaross@gmail.com>
2018-05-22 13:56:49 -0500

    first query attempt

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Route, Switch } from "react-router-dom";
 //IMPORT COMPONENTS
 import Landing from "./components/Landing/Landing";
 import Dashboard from "./components/Dashboard/Dashboard";
-
+import LoginPage from "./components/queries/GetUserQuery";
 //IMPORT ASSETS AND CSS
 import "./App.css";
 
@@ -12,10 +12,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route
-          path="/"
-          render={() => (this.props.user ? <Dashboard /> : <Landing />)}
-        />
+        <LoginPage />
+        <Switch>
+          <Route
+            path="/"
+            render={() => (this.props.user ? <Dashboard /> : <Landing />)}
+          />
+        </Switch>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,14 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import { BrowserRouter as Router } from "react-router-dom";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+const client = new ApolloClient({ uri: "http://localhost:4891/graphql" });
 
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Router>
+      <App />
+    </Router>
+  </ApolloProvider>,
   document.getElementById("root")
 );
