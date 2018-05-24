8a8212fa8f3d3ded4ac1497136ec80448ce988ef
Michael Plange <michaelpplange@gmail.com>
2018-05-24 15:33:32 -0500

    changed stuff on accident

--- src/components/hoc/TextEditor/TextEditor.js
+++ src/components/hoc/TextEditor/TextEditor.js
@@ -24,7 +24,7 @@ const TextEditorContainer = styled.body`
   align-items: center;
   justify-content: center;
   flex-direction: column;
-  height: 60vh;
+  height: 100vh;
 `;
 
 export default class TextEditor extends Component {
