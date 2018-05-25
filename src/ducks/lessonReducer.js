54d4e1cfc7766359a2e5fd7288c0e2bc861e3e71
matthewporter31 <mattporterihsv@gmail.com>
2018-05-25 12:06:00 -0500

    edited some reducers

--- src/ducks/lessonReducer.js
+++ src/ducks/lessonReducer.js
@@ -1,59 +1,69 @@
+const GET_HTML_LESSONS = "GET_HTML_LESSONS";
 const GRADE_HTML_ACTIVITY_1 = "GRADE_HTML_ACTIVITY_1";
 const GRADE_HTML_ACTIVITY_2 = "GRADE_HTML_ACTIVITY_2";
 const GRADE_HTML_ACTIVITY_3 = "GRADE_HTML_ACTIVITY_3";
 
-// !!! DUMMY DATA !!! //
 let initialState = {
   html: {
-    lesson1:
-      "HTML stands for Hyper Text Markup Language. It is the language used to display content on a webpage - like text, images, and links. The basic building block of HTML is an element",
+    lessons: {
+      lesson1:
+        "HTML stands for Hyper Text Markup Language. It is the language used to display content on a webpage - like text, images, and links. The basic building block of HTML is an element",
 
-    lesson2:
-      "Elements are wrapped in tags that tell the browser how to render the element. Examples of tags are headers (<h1>), paragraphs (<p>), images (<img>), and lists (<ul> / <li>). Elements have an opening and closing tag. They look similar, but the closing tag has a forward slash at the beginning. Here's an example of a header element: <h1>Hello World!</h1>",
+      lesson2:
+        "Elements are wrapped in tags that tell the browser how to render the element. Examples of tags are headers (<h1>), paragraphs (<p>), images (<img>), and lists (<ul> / <li>). Elements have an opening and closing tag. They look similar, but the closing tag has a forward slash at the beginning. Here's an example of a header element: <h1>Hello World!</h1>",
 
-    lesson3: `Some elements require attributes to work. Attributes are placed in the opening tag of an element, and add additional functionality to elements like <img>. An image takes two attributes: src (source) and alt (alternative text). The source of an image is the URL or local computer file you want to display, and the alternative text is a custom value that will appear when a user hovers their mouse over an image. Here's an example of an image element:
+      lesson3: `Some elements require attributes to work. Attributes are placed in the opening tag of an element, and add additional functionality to elements like <img>. An image takes two attributes: src (source) and alt (alternative text). The source of an image is the URL or local computer file you want to display, and the alternative text is a custom value that will appear when a user hovers their mouse over an image. Here's an example of an image element:
       
       <img src="http://smileypic.jpg" alt="This shows up when you hover!"></img>
       
       Notice the values for src and alt are wrapped in quotation marks. Anything wrapped in quotes is called a string.`,
 
-    lesson4: `Now that you know how to make a few elements, let's go over on the layout of an HTML document. Every HTML document starts with <!DOCTYPE html>. This line tells a browser to read a document as HTML. Next, you'll need <html> tags below your doctype declaration. Everything else in your document will be contained within these two html tags. The next step is to create an opening and closing <head> tag in HTML. The head portion of your document contains data about your website - like what displays in a browser tab with <title>. Finally, outside of the <head> but still within the <html> tags, we use <body> to contain our website's elements. `,
-    activity1: {
-      id: 1,
-      open: "<h1>",
-      close: "</h1>",
-      success: "Hello World! You're now officially a coder!",
-      fail: "Try again! Your h1 element should be structured like ours above",
-      flag: false // if student successfully completes the activity, this becomes true
+      lesson4: `Now that you know how to make a few elements, let's go over on the layout of an HTML document. Every HTML document starts with <!DOCTYPE html>. This line tells a browser to read a document as HTML. Next, you'll need <html> tags below your doctype declaration. Everything else in your document will be contained within these two html tags. The next step is to create an opening and closing <head> tag in HTML. The head portion of your document contains data about your website - like what displays in a browser tab with <title>. Finally, outside of the <head> but still within the <html> tags, we use <body> to contain our website's elements. `
     },
+    activities: {
+      activity1: {
+        id: 1,
+        open: "<h1>",
+        close: "</h1>",
+        success: "Hello World! You're now officially a coder!",
+        fail: "Try again! Your h1 element should be structured like ours above",
+        flag: false // if student successfully completes the activity, this becomes true
+      },
 
-    activity2: {
-      id: 2,
-      open: "<p>",
-      close: "</p>",
-      success:
-        "Nice work! Now you know how to make haeders and paragraphs for a website.",
-      fail:
-        "Close - try again. This time, the element should look like your h1 element, but with p tags",
-      flag: false
-    },
+      activity2: {
+        id: 2,
+        open: "<p>",
+        close: "</p>",
+        success:
+          "Nice work! Now you know how to make haeders and paragraphs for a website.",
+        fail:
+          "Close - try again. This time, the element should look like your h1 element, but with p tags",
+        flag: false
+      },
 
-    activity3: {
-      id: 3,
-      openUL: "<ul>",
-      closeUL: "</ul>",
-      openLI: "<li>",
-      closeLI: "</li>",
-      success:
-        "Nice list! Now you know how to render three HTML elements. Next we'll learn how to render images.",
-      fail:
-        "Almost! Check out my list of favorite foods and see what you missed.",
-      flag: false
+      activity3: {
+        id: 3,
+        openUL: "<ul>",
+        closeUL: "</ul>",
+        openLI: "<li>",
+        closeLI: "</li>",
+        success:
+          "Nice list! Now you know how to render three HTML elements. Next we'll learn how to render images.",
+        fail:
+          "Almost! Check out my list of favorite foods and see what you missed.",
+        flag: false
+      }
     }
   }
 };
 
-// HTML ACTIVITIES //
+// LESSON FUNCTIONS //
+
+export function getHtmlLessons() {
+  return { type: GET_HTML_LESSONS };
+}
+
+// HTML ACTIVITY FUNCTIONS //
 
 // 1 - In the text editor, try creating your own h1 element. Rendering ‘Hello World!’ is a rite of passage for coders, so put that in your header
 // On Fail - Whoops! Try again. Your h1 element should be structured like ours above.
@@ -100,6 +110,8 @@ export function gradeHtmlActivity3(userInput) {
 
 export default function lessonReducer(state = initialState, action) {
   switch (action.type) {
+    case `${GET_HTML_LESSONS}`:
+      return state.html.lessons;
     case `${GRADE_HTML_ACTIVITY_1}`:
       let userInput = action.payload.trim();
       if (
