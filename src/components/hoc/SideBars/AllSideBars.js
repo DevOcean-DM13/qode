3f12f43dd72750d7ca10b9f5ae0ec740ffd85c6a
Michael Plange <michaelpplange@gmail.com>
2018-05-24 22:40:14 -0500

    late night, i made no changes, just pulling

--- src/components/hoc/SideBars/AllSideBars.js
+++ src/components/hoc/SideBars/AllSideBars.js
@@ -40,8 +40,10 @@ export const SignUpSideBar = createSideBar(SignUp);
 
 // anotha one
 const FirstQuizTitle = styled.h1`
-  font-family: "Noto Sans", sans-serif;
+  font-family: "Tajawal", sans-serif;
+  font-weight: 700;
 `;
+const TextBox = styled.div``;
 class EleAndTag extends Component {
   render() {
     return (
